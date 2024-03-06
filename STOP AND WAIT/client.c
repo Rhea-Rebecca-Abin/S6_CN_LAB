af794f7760a512d2f748687f97a4323eb38e6134
Rhea-Rebecca-Abin <114933201+Rhea-Rebecca-Abin@users.noreply.github.com>
2024-03-06 15:49:19 +0530

    Add files via upload

---
+++ STOP AND WAIT/client.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<string.h>
+#include<arpa/inet.h>
+#define PORT 8000
+
+typedef struct packet
+{
+    char data[1024];
+} Packet;
+
+typedef struct frame
+{
+    int frame_kind;
+    int sq_no;
+    int ack;
+    Packet packet;
+} Frame;
+
+int main()
+{
+    int sockfd;
+    struct sockaddr_in servaddr;
+    char buffer[1024];
+
+    int frame_id=0;
+    Frame frame_recv;
+    Frame frame_send;
+    int ack_recv=1;
+
+    sockfd=socket(AF_INET,SOCK_DGRAM,0);
+    servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_port=htons(PORT);
+
+    while(1)
+    {
+        if(ack_recv==1)
+        {
+            frame_send.sq_no=frame_id;
+            frame_send.frame_kind=1;
+            frame_send.ack=0;
+
+            printf("Enter Data : ");
+            scanf("%s",buffer);
+            strcpy(frame_send.packet.data,buffer);
+            sendto(sockfd,&frame_send,sizeof(Frame),0,(struct sockaddr*)&servaddr,sizeof(servaddr));
+            printf("[+]Frame Send\n");
+        }
+        int addr_size=sizeof(servaddr);
+        int f_recv_size=recvfrom(sockfd,&frame_recv,sizeof(frame_recv),0,(struct sockaddr*)&servaddr,&addr_size);
+        if(f_recv_size>0&&frame_recv.sq_no==0&&frame_recv.ack==frame_id+1)
+        {
+            printf("[+]Ack Recieved\n");
+            ack_recv=1;
+        }
+        else
+        {
+            printf("[-]Ack Not Recieved\n");
+            ack_recv=0;
+        }
+        frame_id++;
+    }
+    close(sockfd);
+    return 0;
+}
---
+++ STOP AND WAIT/server.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#define PORT 8000
+
+typedef struct packet
+{
+    char data[1024];
+} Packet;
+
+typedef struct frame
+{
+    int frame_kind;
+    int sq_no;
+    int ack;
+    Packet packet;
+} Frame;
+
+int main()
+{
+    int sockfd, addr_size;
+    struct sockaddr_in servaddr, cli;
+    char buffer[1024];
+
+    int frame_id = 0;
+    Frame frame_recv, frame_send;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+
+    bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    addr_size = sizeof(cli);
+    while (1)
+    {
+        int f_recv_size = recvfrom(sockfd, &frame_recv, sizeof(Frame), 0, (struct sockaddr *)&cli, &addr_size);
+        if (f_recv_size > 0 && frame_recv.frame_kind == 1 && frame_recv.sq_no == frame_id)
+        {
+            printf("[+]Frame Recieved : %s\n", frame_recv.packet.data);
+
+            frame_send.sq_no = 0;
+            frame_send.frame_kind = 0;
+            frame_send.ack = frame_recv.sq_no + 1;
+            sendto(sockfd, &frame_send, sizeof(frame_send), 0, (struct sockaddr *)&cli, addr_size);
+            printf("[+]Ack Send\n");
+        }
+        else
+        {
+            printf("[+]Frame Not Recieved\n");
+        }
+        frame_id++;
+    }
+    close(sockfd);
+    return 0;
+}
