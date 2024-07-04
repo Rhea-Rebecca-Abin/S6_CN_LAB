a43c2e7bddececb96cdb87e460996c9fd50000c3
Rhea Rebecca Abin <114933201+Rhea-Rebecca-Abin@users.noreply.github.com>
2024-07-04 14:53:43 +0530

    Add files via upload

---
+++ TCP_FIBONACCI/client.c
@@ -0,0 +1,53 @@
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<strings.h>
+#include<unistd.h>
+int main()
+{
+    int clientsocket,port; 
+    struct sockaddr_in serveraddr; 
+    socklen_t len; 
+    char message[500];
+
+    int number;
+
+
+    clientsocket=socket(AF_INET,SOCK_STREAM,0);
+    bzero((char*)&serveraddr,sizeof(serveraddr));
+    len=sizeof(serveraddr); 
+    serveraddr.sin_family=AF_INET; 
+
+    printf("Enter the port number ");
+    scanf("%d",&port);
+    serveraddr.sin_port=htons(port);
+    printf("\nTrying to connect to the server.\n");
+
+
+    connect(clientsocket,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+    printf("\nConnected to the server.\n");
+
+    recv(clientsocket,message,sizeof(message),0);
+    printf("\n%s ",message);
+
+    scanf("%d",&number);
+    send(clientsocket,&number,sizeof(number),0);
+
+    recv(clientsocket,message,sizeof(message),0);
+    printf("\n%s ",message);
+
+
+    close(clientsocket);
+}
+/*OUTPUT
+Enter the port number 2000
+
+Trying to connect to the server.
+
+Connected to the server.
+
+Enter an integer: 8
+
+0 1 1 2 3 5 8 13*/
